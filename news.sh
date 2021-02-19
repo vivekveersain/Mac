4ab7250ea00efce55753bf266e91698ae1af00b4
Vivek Arya <vivekveersainarya@gmail.com>
2021-02-19 06:32:57 +0530

    Minor changes!

--- news.sh
+++ news.sh
@@ -12,7 +12,6 @@ scrapper()
 recipes_at="/Users/vivekarya/Library/Preferences/calibre/custom_recipes/"
 news_folder="/Users/vivekarya/Downloads/News"
 
-rm -r $news_folder
 mkdir $news_folder
 cd $recipes_at
 
@@ -28,6 +27,7 @@ zip $news_folder/News.zip $news_folder/*
 
 osascript -e 'display notification "Emailing..." with title "News"'
 echo "E-Mailing..."
-#/Applications/Calibre.app/Contents/MacOS/calibre-smtp --relay 'smtp.live.com' --port 587 --username 'vivek.chaudhary@live.com' --password "$pwd" --encryption-method 'TLS' --subject 'News' 'vivek.chaudhary@live.com' 'vivekveersain@kindle.com' 'Enjoy.' -a $news_folder/News.zip
 /Applications/Calibre.app/Contents/MacOS/calibre-smtp --subject 'News' 'vivek.chaudhary@live.com' 'vivekveersain@kindle.com' 'Enjoy.' -a $news_folder/News.zip
+
 osascript -e 'display notification "Cleaning..." with title "News"'
+rm -r $news_folder
